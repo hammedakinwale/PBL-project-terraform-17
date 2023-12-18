adb3f6c1859cbb7ae11b3d9d75b025549cc649b3
Hammed Akinwale <78992096+hammedakinwale@users.noreply.github.com>
2023-12-18 11:30:52 +0100

    Update security.tf

--- security.tf
+++ security.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "ext-alb-sg" {
 
   ingress {
     description = "HTTPS"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -36,10 +36,27 @@ resource "aws_security_group" "ext-alb-sg" {
 
 }
 
+resource "aws_security_group_rule" "inbound-alb-http" {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.ext-alb-sg.id
+}
+
+resource "aws_security_group_rule" "inbound-alb-https" {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+    type = "ingress"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.ext-alb-sg.id
+}
 
 # security group for bastion, to allow access into the bastion host from you IP
 resource "aws_security_group" "bastion_sg" {
-  name        = "vpc_web_sg"
+  name        = "bastion_sg"
   vpc_id = aws_vpc.main.id
   description = "Allow incoming HTTP connections."
 
@@ -57,11 +74,13 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-   tags = merge(
+  
+#   vpc_id = aws_vpc.main.id 
+  tags = merge(
     var.tags,
     {
       Name = "Bastion-SG"
+      Environment = var.environment
     },
   )
 }
